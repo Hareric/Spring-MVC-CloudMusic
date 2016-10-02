a21713a70bbcb8e99ab691481566a0496517e1d7
Eric Chan <hareric@qq.com>
2016-10-02 22:16:06 +0800

    CH2

---
+++ src/CH2/Travel.java
@@ -0,0 +1,64 @@
+package CH2;
+abstract class TravelSuper 
+{
+	String travelMethod;
+	abstract void howToTravel();
+}
+
+class TravelCar extends TravelSuper
+{
+	public void howToTravel()
+	{
+		super.travelMethod = "开车去旅行";
+	}
+}
+
+class TravelTrain extends TravelSuper
+{
+	public void howToTravel()
+	{
+		super.travelMethod = "搭火车去旅行";
+	}
+}
+
+class TravelPlane extends TravelSuper
+{
+	public void howToTravel()
+	{
+		super.travelMethod = "搭飞机去旅行";
+	}
+}
+
+/**
+ * 简单工厂与策略模式的结合
+ */
+class TravelContext
+{
+	TravelSuper ts = null;
+	TravelContext(String type)
+	{
+		switch(type)
+		{
+			case "plane": ts = new TravelPlane();break;
+			case "train": ts = new TravelTrain();break;
+			case "car": ts = new TravelCar();break;
+		}
+	}
+	
+	String getResult()
+	{
+		ts.howToTravel();
+		return ts.travelMethod;
+	}
+}
+
+public class Travel 
+{
+	public static void main(String args[])
+	{
+		String plan = "car";  // plane car
+		TravelContext tc = new TravelContext(plan);
+		System.out.println(tc.getResult());
+	}
+
+}
